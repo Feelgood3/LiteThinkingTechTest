cc2a4be1d3d578a263165b4e186abf0b71ae087e
Sergio Moncada <sergiomoncadao@gmail.com>
2023-02-06 22:25:11 -0500

    feat/main site styles formatting ready

--- rick-and-morty-app/src/App.js
+++ rick-and-morty-app/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  let APIURL = `https://rickandmortyapi.com/api/character/?page=1`
+
+  const [characters, setCharacters] = useState([])
+
+  const requiredItems = async() => {
+    const response = await fetch(APIURL)
+    const data = await response.json()
+    setCharacters(data.results)
+    console.log(data.results)
+}
+
+useEffect(() => {
+    requiredItems()
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <div className='main-container-title'>
+          <h1 className='title'>Rick and Morty Characters</h1>
+        </div>
+        <div className="filter">Search bar goes here</div>
+        <div className='main-container-cards'>
+          <div className="card-container">test</div>
+          <div className="card-container">test2</div>
+          <div className="card-container">test3</div>
+          <div className="card-container">test4</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+          <div className="card-container">test5</div>
+        </div>
+      </div>
     </div>
   );
 }
